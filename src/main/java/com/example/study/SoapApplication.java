47ba4bd52f885482db5069792c4fe5916ee6b0c6
Lucas <lstorres093@gmail.com>
2022-08-15 14:17:47 +0100

    creating project

---
+++ src/main/java/com/example/study/SoapApplication.java
@@ -0,0 +1,14 @@
+package com.example.study;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoapApplication.class, args);
+		System.out.println("teste");
+	}
+
+}
