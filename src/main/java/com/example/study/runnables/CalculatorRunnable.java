4a434b90fca5d4c0fa472f431dad3ce08fb501a8
Lucas <lstorres093@gmail.com>
2022-08-15 20:19:32 +0100

    Creating CalculatorRunnable to test the Web Service

--- src/main/java/com/example/study/runnables/CalculatorRunnable.java
+++ src/main/java/com/example/study/runnables/CalculatorRunnable.java
@@ -1,2 +1,20 @@
-package com.example.study.runnables;public class CalculatorRunnable {
+package com.example.study.runnables;
+
+import com.example.study.WSClient.CalculatorClient;
+import com.test.calculator.client.gen.AddResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CalculatorRunnable implements CommandLineRunner {
+
+    @Autowired
+    public CalculatorClient calculatorClient;
+
+    @Override
+    public void run(String... args) throws Exception {
+        AddResponse response = calculatorClient.addTwoNumbers(2, 3);
+        System.out.println("Resposta: "+response.getAddResult());
+    }
 }
